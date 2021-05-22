18e435eb679590540d7b6f2c7518e3da99765963
daudi13 <davouma13@gmail.com>
2021-05-22 04:57:51 +0300

    commit 'working' code

---
+++ app.js
@@ -0,0 +1,28 @@
+
+const theContainer = document.querySelector('body');
+
+window.addEventListener('keydown', (e) => {
+    theContainer.innerHTML = `<div class="container">
+        <div class="showCode" id="showCode">${e.code}</div        <div class="meanings">
+            <div class="box">
+                <p>event.Key</p>
+                <div class="box-meaning">
+                    <div id="eventKey" class="event">${e.key === ' ' ? 'space': e.key}</div>
+                </div>
+            </div>
+            <div class="box">
+                <p>event.Key</p>
+                <div class="box-meaning">
+                    <div id="eventKeyCode" class="event">${e.keyCode}</div>
+                </div>
+            </div>
+            <div class="box">
+                <p>event.Key</p>
+                <div class="box-meaning">
+                    <div id="eventcode" class="event">${e.code}</div>
+                </div>
+            </div>
+        </div>
+    </div>`
+    console.log(e);
+})
